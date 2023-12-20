8c07fea6a5d174c598e0ed299bd03a242862c9e3
Nishant7620 <nkaranjavkar72@gmail.com>
2023-12-20 22:19:28 +0530

    updated

--- replacing cur_char with new_char.py
+++ replacing cur_char with new_char.py
@@ -3,3 +3,15 @@
 #The match must be case-sensitive (do not replace lowercase letters if curr_char is uppercase).
 #If no match is found, print the initial string.
 
+str = input("enter a string: ")
+curr_char = input("enter charater you want to replace: ")
+new_char = input("enter charater you want to add: ")
+
+if new_char not in str:
+    print(str)
+
+else:
+    if new_char in str:
+        new_string = str.replace(curr_char,new_char)
+
+        print(new_string)
