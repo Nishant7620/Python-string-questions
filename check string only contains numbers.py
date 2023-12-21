f96daa14f0a242e04af6473387582c7df035e2e4
Nishant7620 <nkaranjavkar72@gmail.com>
2023-12-21 23:32:34 +0530

    updated

--- check string only contains numbers.py
+++ check string only contains numbers.py
@@ -1,3 +1,10 @@
 #Write a Python program that check if a string only contains numbers. If it does, print True. Else, print False.
 
 str1 = input("enter a string: ")
+
+if str1.isnumeric():
+    print("true")
+
+else:
+    print("false")
+    
